f4abf1a06ac044b7225d785f0a6b042560145948
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-02-17 21:46:52 +0200

    clear shapes

--- core/src/shapes.rs
+++ core/src/shapes.rs
@@ -4,6 +4,7 @@ use crossbeam_channel::{Receiver, Sender, bounded};
 use std::fs;
 use std::ops::Range;
 use std::path::PathBuf;
+use std::sync::Arc;
 use std::time::Instant;
 use steel::steel_vm::engine::Engine;
 use steel::steel_vm::register_fn::RegisterFn;
@@ -12,10 +13,11 @@ pub struct ShapesPlugin;
 
 impl Plugin for ShapesPlugin {
     fn build(&self, app: &mut App) {
-        let (tx, rx) = bounded::<Shape>(100);
+        let (tx, rx) = bounded::<ShapeCommand>(100);
         app.init_resource::<Shapes>()
             .insert_resource(ShapeReciver(rx))
-            .insert_resource(ShapeSender(tx))
+            .insert_resource(ShapeSender(Arc::new(tx)))
+            .insert_resource(Start(Instant::now()))
             .add_systems(
                 Update,
                 (animate_shapes, re_execute_the_script, animate_new_shapes),
@@ -23,87 +25,132 @@ impl Plugin for ShapesPlugin {
     }
 }
 
-fn re_execute_the_script(keys: Res<ButtonInput<KeyCode>>, sender: Res<ShapeSender>) {
+#[derive(Resource)]
+struct Start(Instant);
+
+fn re_execute_the_script(
+    keys: Res<ButtonInput<KeyCode>>,
+    sender: Res<ShapeSender>,
+    start: Res<Start>,
+) {
     if keys.just_pressed(KeyCode::Enter) {
-        execute_script(sender.0.clone());
+        let sender = sender.0.clone();
+        let start = start.0.clone();
+        std::thread::spawn(move || {
+            execute_script(sender, start);
+        });
     }
 }
 
-fn execute_script(sender: Sender<Shape>) {
-    std::thread::spawn(move || {
-        let mut args = std::env::args();
-        let program_name = args.next().expect("program must exist!!");
-        let path = match args.next().and_then(|x| x.parse::<PathBuf>().ok()) {
-            Some(path) => path,
-            None => {
-                eprintln!("Usage: {} <script.scm>", program_name);
-                std::process::exit(1);
-            }
-        };
-
-        let mut engine = Engine::new();
-
-        let sender2 = sender.clone();
-        let sender3 = sender.clone();
-        engine.register_fn(
-            "SinShape",
-            move |amplitude: i64, frequency: i64, range_begin: i64, range_end: i64| {
-                let ss = SinShape {
-                    begin: Instant::now(),
-                    verts: Vec::new(),
-                    amplitude: amplitude as f32,
-                    frequency: frequency as f32,
-                    range: range_begin as f32..range_end as f32,
-                };
-                sender2.send(Shape::Sin(dbg!(ss))).unwrap();
-            },
-        );
-        engine.register_fn(
-            "CircleShape",
-            move |radius: i64, x: i64, y: i64, z: i64, speed: i64| {
-                let ss = CircleShape {
-                    begin: Instant::now(),
-                    angle: 0.0,
-                    radius: radius as f32,
-                    center: Vec3 {
-                        x: x as f32,
-                        y: y as f32,
-                        z: z as f32,
-                    },
-                    speed: speed as f32,
-                };
-                sender3.send(Shape::Circle(dbg!(ss))).unwrap();
-            },
-        );
-        engine.register_fn("ClearShapes", move || {
-            sender.send(Shape::Clear).unwrap();
-        });
-
-        let script = fs::read_to_string(&path).unwrap();
-        println!("Executing: {}\n", path.display());
-
-        match engine.run(script) {
-            Ok(results) => {
-                println!("--- Results ---");
-                for (i, val) in results.iter().enumerate() {
-                    println!("[{i}] {val:?}");
-                }
-            }
-            Err(e) => eprintln!("Error during execution: {}", e),
+fn execute_script(sender: Arc<Sender<ShapeCommand>>, start: Instant) {
+    let mut args = std::env::args();
+    let program_name = args.next().expect("program must exist!!");
+    let path = match args.next().and_then(|x| x.parse::<PathBuf>().ok()) {
+        Some(path) => path,
+        None => {
+            eprintln!("Usage: {} <script.scm>", program_name);
+            std::process::exit(1);
         }
+    };
+
+    let mut engine = Engine::new();
+
+    let sender2 = sender.clone();
+    let sender3 = sender.clone();
+    let sender4 = sender.clone();
+    engine.register_fn(
+        "SinShape",
+        move |amplitude: i64, frequency: i64, range_begin: i64, range_end: i64| {
+            let ss = SinShape::new(
+                amplitude as f32,
+                frequency as f32,
+                range_begin as f32..range_end as f32,
+            );
+            let shape = Shape::sin(ss, start);
+            let id = shape.id;
+            let ss = ShapeCommand::Draw(shape);
+            sender2.send(ss).unwrap();
+            return id;
+        },
+    );
+    engine.register_fn(
+        "CircleShape",
+        move |radius: i64, x: i64, y: i64, z: i64, speed: i64| {
+            let ss = CircleShape {
+                angle: 0.0,
+                radius: radius as f32,
+                center: Vec3 {
+                    x: x as f32,
+                    y: y as f32,
+                    z: z as f32,
+                },
+                speed: speed as f32,
+            };
+            let shape = Shape::circle(ss, start);
+            let id = shape.id;
+            let ss = ShapeCommand::Draw(shape);
+            sender3.send(ss).unwrap();
+            return id;
+        },
+    );
+    engine.register_fn("ClearShape", move |id: usize| {
+        sender4.send(ShapeCommand::ClearShapeWithId(id)).unwrap();
     });
+    engine.register_fn("ClearShapes", move || {
+        sender.send(ShapeCommand::ClearAll).unwrap();
+    });
+
+    let script = fs::read_to_string(&path).unwrap();
+    println!("Executing: {}\n", path.display());
+
+    match engine.run(script) {
+        Ok(results) => {
+            println!("--- Results ---");
+            for (i, val) in results.iter().enumerate() {
+                println!("[{i}] {val:?}");
+            }
+        }
+        Err(e) => eprintln!("Error during execution: {}", e),
+    }
 }
 
 #[derive(Resource)]
-struct ShapeReciver(Receiver<Shape>);
+struct ShapeReciver(Receiver<ShapeCommand>);
 
 #[derive(Resource)]
-struct ShapeSender(Sender<Shape>);
+struct ShapeSender(Arc<Sender<ShapeCommand>>);
 
-enum Shape {
+enum ShapeCommand {
+    ClearAll,
+    Draw(Shape),
+    ClearShapeWithId(usize),
+}
+
+struct Shape {
+    id: u128,
+    instant: Instant,
+    form: ShapeForm,
+}
+
+impl Shape {
+    fn sin(s: SinShape, start: Instant) -> Self {
+        let form = ShapeForm::Sin(s);
+        let instant = Instant::now();
+        let id = instant.duration_since(start).as_nanos();
+        Self { id, instant, form }
+    }
+
+    fn circle(s: CircleShape, start: Instant) -> Self {
+        let form = ShapeForm::Circle(s);
+        let instant = Instant::now();
+        let id = instant.duration_since(start).as_nanos();
+        Self { id, instant, form }
+    }
+}
+
+enum ShapeForm {
     Sin(SinShape),
     Circle(CircleShape),
-    Clear,
 }
 
 #[derive(Resource, Default)]
@@ -111,7 +158,6 @@ struct Shapes(Vec<Shape>);
 
 #[derive(Debug)]
 struct CircleShape {
-    begin: Instant,
     angle: f32,
     radius: f32,
     center: Vec3,
@@ -121,7 +167,6 @@ struct CircleShape {
 impl Default for CircleShape {
     fn default() -> Self {
         Self {
-            begin: Instant::now(),
             angle: 0.,
             radius: 2.,
             center: Vec3::ZERO,
@@ -139,20 +184,23 @@ fn animate_shapes(mut gizmos: Gizmos, mut shapes: ResMut<Shapes>) {
 fn animate_new_shapes(mut shapes: ResMut<Shapes>, reciver: Res<ShapeReciver>) {
     for shape in reciver.0.try_iter() {
         match shape {
-            Shape::Clear => {
+            ShapeCommand::ClearAll => {
                 shapes.0.clear();
             }
-            shape => {
+            ShapeCommand::Draw(shape) => {
                 shapes.0.push(shape);
+            }
+            ShapeCommand::ClearShapeWithId(id) => {
+                shapes.0.retain(|x| x.id != id as u128);
             }
         }
     }
 }
 
 fn handle_shape(gizmos: &mut Gizmos<'_, '_>, shape: &mut Shape) {
-    match shape {
-        Shape::Sin(ss) => {
-            let x = ss.begin.elapsed().as_secs_f32();
+    match &mut shape.form {
+        ShapeForm::Sin(ss) => {
+            let x = shape.instant.elapsed().as_secs_f32();
             let start = ss.range.start;
             let end = ss.range.end;
             let x = start + x;
@@ -162,8 +210,8 @@ fn handle_shape(gizmos: &mut Gizmos<'_, '_>, shape: &mut Shape) {
             }
             gizmos.linestrip(ss.verts.clone(), GREEN);
         }
-        Shape::Circle(cs) => {
-            let angle = cs.begin.elapsed().as_secs_f32() * cs.speed;
+        ShapeForm::Circle(cs) => {
+            let angle = shape.instant.elapsed().as_secs_f32() * cs.speed;
             if angle <= 363. {
                 cs.angle = angle;
             }
@@ -177,25 +225,31 @@ fn handle_shape(gizmos: &mut Gizmos<'_, '_>, shape: &mut Shape) {
                 )
                 .resolution(1000);
         }
-        Shape::Clear => (),
     }
 }
 
 #[derive(Debug)]
 struct SinShape {
-    begin: Instant,
     verts: Vec<Vec3>,
     amplitude: f32,
     frequency: f32,
     range: Range<f32>,
 }
 
-impl SinShape {}
+impl SinShape {
+    fn new(amplitude: f32, frequency: f32, range: Range<f32>) -> Self {
+        Self {
+            verts: Vec::new(),
+            amplitude,
+            frequency,
+            range,
+        }
+    }
+}
 
 impl Default for SinShape {
     fn default() -> Self {
         Self {
-            begin: Instant::now(),
             verts: Default::default(),
             amplitude: 3.,
             frequency: 3.,
