94ce088c3415ceb80bc07647e0b150064244b802
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-03-20 18:12:56 +0200

    vite config

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        // Removes hash from main entry points
+        entryFileNames: `[name].js`,
+        // Removes hash from split chunks
+        chunkFileNames: `[name].js`,
+        // Removes hash from static assets (images, css, etc.)
+        assetFileNames: `[name].[ext]`
+      },
+    },
+  }
+})
